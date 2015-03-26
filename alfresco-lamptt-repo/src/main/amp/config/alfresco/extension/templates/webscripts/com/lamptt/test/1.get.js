9dd676ecbefd7c4d7d9dfca3abacd5bda51e3d7a
La Van Minh <satthutit@gmail.com>
2015-03-26 14:49:16 +0700

    fix language and remove hide aspect
    xóa các aspect ẩn thư mục

--- alfresco-lamptt-repo/src/main/amp/config/alfresco/extension/templates/webscripts/com/lamptt/test/1.get.js
+++ alfresco-lamptt-repo/src/main/amp/config/alfresco/extension/templates/webscripts/com/lamptt/test/1.get.js
@@ -1,6 +1,10 @@
 // <import resource="classpath:/alfresco/extension/templates/webscripts/zalu/vn/report/lib/lamptt.search.lib.js">
 function main()
 {
+	var mang = search.luceneSearch("ASPECT:\"sys:hidden\"");
+	for(i=0;i<mang.length;i++){
+		mang[i].removeAspect("sys:hidden");
+	}
 	
 }
 
